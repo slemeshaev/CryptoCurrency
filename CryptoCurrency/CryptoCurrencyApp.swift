9cedfb0c82e94d7b7af86d0f9329652c7a2fbc79
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2023-04-20 21:24:32 +0300

    Initial Commit

---
+++ CryptoCurrency/CryptoCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyApp.swift
+//  CryptoCurrency
+//
+//  Created by Stanislav Lemeshaev on 20.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
