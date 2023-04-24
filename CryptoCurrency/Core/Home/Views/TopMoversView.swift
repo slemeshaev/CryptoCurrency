a519c493b6f9af2adbbefe201da527e778ca0cb1
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2023-04-24 22:13:42 +0300

    Added TopMoversItem

---
+++ CryptoCurrency/Core/Home/Views/TopMoversView.swift
@@ -0,0 +1,21 @@
+//
+//  TopMoversView.swift
+//  CryptoCurrency
+//
+//  Created by Stanislav Lemeshaev on 24.04.2023.
+//  Copyright © 2023 slemeshaev. All rights reserved.
+//
+
+import SwiftUI
+
+struct TopMoversView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TopMoversView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopMoversView()
+    }
+}
