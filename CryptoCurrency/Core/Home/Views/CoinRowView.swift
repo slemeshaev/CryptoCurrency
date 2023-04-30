262c746eda3a380739aa5cf4c3cdf55e7f65c0f1
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2023-04-30 20:10:03 +0300

    Set KingFisher
    - Added url for coin icons

--- CryptoCurrency/Core/Home/Views/CoinRowView.swift
+++ CryptoCurrency/Core/Home/Views/CoinRowView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct CoinRowView: View {
     let coin: Coin
@@ -17,7 +18,7 @@ struct CoinRowView: View {
                 .font(.caption)
                 .foregroundColor(.gray)
             
-            Image(systemName: "bitcoinsign.circle.fill")
+            KFImage(URL(string: coin.image))
                 .resizable()
                 .scaledToFit()
                 .frame(width: 32, height: 32)
--- CryptoCurrency/Core/Home/Views/TopMoversItemView.swift
+++ CryptoCurrency/Core/Home/Views/TopMoversItemView.swift
@@ -7,13 +7,14 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct TopMoversItemView: View {
     let coin: Coin
     
     var body: some View {
         VStack(alignment: .leading) {
-            Image(systemName: "bitcoinsign.circle.fill")
+            KFImage(URL(string: coin.image))
                 .resizable()
                 .frame(width: 32, height: 32)
                 .foregroundColor(.orange)
